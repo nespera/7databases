9b7a719fa492e10506688e6007a19eb3f7dff763
Chris Rimmer <c@24.org.uk>
2012-09-15 08:50:35 +0100

    Added broken sibling code and readme

---
+++ neo4j/README.txt
@@ -0,0 +1,5 @@
+From the gremlin console, you can do this, to load the graphml file:
+g.loadGraphML('/path/to/file.graphml')
+
+to load in the Groovy script do this:
+load /path/to/groovy/script
--- neo4j/sibling.groovy
+++ neo4j/sibling.groovy
@@ -5,3 +5,11 @@ Gremlin.defineStep('sibling',
     _().sideEffect{start = it}.in('parent_of').out('parent_of').filter{it.name != start.name}.dedup
   }
 )
+
+Gremlin.defineStep('sibling2',
+  [Vertex, Pipe],
+  {
+    def start
+    _().sideEffect{start = it}.in('parent_of').out('parent_of').except([start]).dedup
+  }
+)
